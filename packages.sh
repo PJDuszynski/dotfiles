9352696176c3d60d426daf342c5d2caf574fbfb9
PJDuszynski <scottduszy@proton.me>
2023-02-12 23:55:24 -0600

    wip: add more packages & categories

--- packages.sh
+++ packages.sh
@@ -1,16 +1,31 @@
-
-
-
-
 export AUR_packages=(
     # Productivity Tools
     betterbird-bin
     todotxt
+    tutanota-desktop
+    electronmail-bin
+    zsh-theme-powerlevel10k-git
 )
 
 export pacman_packages=(
     # Browser
     vivaldi
     gnupg
+    # Go
+    go
+    shfmt
 
+)
+
+export pip_packages=(
+    pipx
+)
+
+export pipx_packages=(
+    black
+    pylint
+    flake8
+    radon
+    poetry
+    pytest
 )
